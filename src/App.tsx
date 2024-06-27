ec4c66d3a58122aa2313ff93c6724371b9e233e0
Alice R <alicepr@yandex.ru>
2024-06-28 02:48:37 +0300

    app: create global styles and initial routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './pages/not-found-page/component';
+import { Layout } from './widgets/layout/component';
+import { MoviePage } from './pages/movie/component';
+import { MoviesPage } from './pages/movies/component';
+import { movie } from './shared/mock';
+
 export const App = () => {
-	return <div>Hello</div>;
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route
+						index
+						element={<MoviesPage movies={[movie, movie, movie]} />}
+					/>
+					<Route path='/movie:id' element={<MoviePage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
 };
