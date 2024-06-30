fdc690c9265c2bfcca04c3e8286e1fe32badc362
Alice R <alicepr@yandex.ru>
2024-06-30 14:13:34 +0300

    filter: create custom select, update filter

--- src/shared/ui/select/component.tsx
+++ src/shared/ui/select/component.tsx
@@ -1,45 +1,90 @@
-import { ChangeEvent } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
+import classNames from 'classnames';
 
 type SelectProps = {
-	name: string;
-	id: string;
 	title: string;
 	values: Record<string, string>;
 	currentValue?: string | null;
-	onChange?: (event: ChangeEvent<HTMLSelectElement>) => void;
+	onChange: (value: string) => void;
 };
 
 export const Select = ({
-	name,
-	id,
 	title,
 	values,
-	currentValue = 'DEFAULT',
+	currentValue,
 	onChange,
 }: SelectProps) => {
-	console.log(currentValue);
+	const [selectedValue, setSelectedValue] = useState(
+		currentValue ? values[currentValue] : currentValue
+	);
+	const [isVisible, setIsVisible] = useState(false);
+	const filterRef = useRef(null);
+
+	useEffect(() => {
+		const onEsc = (evt: KeyboardEvent) => {
+			if (evt.key === 'Escape') {
+				setIsVisible(false);
+			}
+		};
+		const onClick = (evt: Event) => {
+			evt.stopPropagation();
+			console.log('click');
+			if (isVisible) {
+				setIsVisible(false);
+			}
+		};
+		document.addEventListener('click', onClick);
+		document.addEventListener('keydown', onEsc);
+		return () => {
+			document.removeEventListener('keydown', onEsc);
+			document.removeEventListener('click', onClick);
+		};
+	}, [isVisible]);
+
 	return (
-		<>
-			<label htmlFor={id}>{title}</label>
+		<div className={styles.container} ref={filterRef.current}>
+			<h3 className={styles.title}>{title}</h3>
 			<div className={styles.selectWrapper}>
-				<select
-					name={name}
-					id={id}
-					defaultValue={currentValue || 'DEFAULT'}
-					className={styles.select}
-					onChange={onChange}
+				<div
+					className={classNames(styles.select, {
+						[styles.active]: isVisible,
+					})}
+					onClick={(evt) => {
+						evt.stopPropagation();
+						setIsVisible(!isVisible);
+					}}
 				>
-					<option value='DEFAULT' disabled>
-						{`Выберите ${title.split(' ')[0].toLocaleLowerCase()}`}
-					</option>
-					{Object.entries(values).map(([key, value], index) => (
-						<option key={index} value={key} selected={currentValue === key}>
-							{value}
-						</option>
-					))}
-				</select>
+					{!currentValue
+						? `Выберите ${title.split(' ')[0].toLocaleLowerCase()}`
+						: values[currentValue]}
+					<span
+						className={classNames(styles.arrow, {
+							[styles.upArrow]: isVisible,
+						})}
+					></span>
+				</div>
+				{isVisible && (
+					<div className={styles.options}>
+						{Object.entries(values).map(([key, value], index) => (
+							<div
+								key={index}
+								className={classNames(styles.option, {
+									[styles.selected]: key === selectedValue,
+								})}
+								onClick={(evt) => {
+									evt.stopPropagation();
+									setSelectedValue(value);
+									onChange(key);
+									setIsVisible(false);
+								}}
+							>
+								{value}
+							</div>
+						))}
+					</div>
+				)}
 			</div>
-		</>
+		</div>
 	);
 };
--- src/widgets/filter/component.tsx
+++ src/widgets/filter/component.tsx
@@ -1,4 +1,3 @@
-import { ChangeEvent } from 'react';
 import { Select } from '../../shared/ui/select/component';
 import { GENRES, GenresEn, YearKeys, YEARS } from './config';
 import styles from './styles.module.css';
@@ -14,24 +13,18 @@ export const Filter = ({ onChange, currGenre, currYear }: FilterProps) => {
 		<div className={styles.container}>
 			<h3 className={styles.title}>Фильтр</h3>
 			<Select
-				id='genre'
-				name='genre'
 				title='Жанр'
 				values={GENRES}
 				currentValue={currGenre}
-				onChange={(event: ChangeEvent<HTMLSelectElement>) => {
-					const genre = event.target?.value;
+				onChange={(genre) => {
 					onChange('genre', genre !== undefined && genre !== '0' ? genre : '');
 				}}
 			/>
 			<Select
-				id='release_year'
-				name='release_year'
 				title='Год выпуска'
 				values={YEARS}
 				currentValue={currYear}
-				onChange={(event: ChangeEvent<HTMLSelectElement>) => {
-					const year = event.target?.value;
+				onChange={(year) => {
 					onChange(
 						'release_year',
 						year !== undefined && year !== '0' ? year : ''
